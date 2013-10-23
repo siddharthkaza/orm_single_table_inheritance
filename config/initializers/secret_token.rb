d9425f4f09e36371b32541de7aa109fbdfd6bec2
siddharthkaza <siddharthkaza@hotmail.om>
2013-10-22 17:58:32 -0700

    with secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrmSingleTableInheritanceRails3214::Application.config.secret_token = '0addced0ef394ecba594806e474681b2c92651f124701ce139404cfe3d9797f69609803cd6916fe27aec13ca7fd112bf11b85f2276ffce3975ee430a02ec80c6'
